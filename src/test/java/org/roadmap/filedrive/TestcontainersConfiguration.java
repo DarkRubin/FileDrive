111ce4b6ecb12abfc76ac836e5b57f6d9ecd6eaa
Vadim <botgugla@gmail.com>
2024-08-23 20:29:21 +0300

    refactor: change latest containers versions to static

--- src/test/java/org/roadmap/filedrive/TestcontainersConfiguration.java
+++ src/test/java/org/roadmap/filedrive/TestcontainersConfiguration.java
@@ -8,18 +8,18 @@ import org.testcontainers.containers.MySQLContainer;
 import org.testcontainers.utility.DockerImageName;
 
 @TestConfiguration(proxyBeanMethods = false)
-class TestcontainersConfiguration {
+public class TestcontainersConfiguration {
 
     @Bean
     @ServiceConnection
     MySQLContainer<?> mysqlContainer() {
-        return new MySQLContainer<>(DockerImageName.parse("mysql:latest"));
+        return new MySQLContainer<>(DockerImageName.parse("mysql:9.0.1"));
     }
 
     @Bean
     @ServiceConnection(name = "redis")
     GenericContainer<?> redisContainer() {
-        return new GenericContainer<>(DockerImageName.parse("redis:latest")).withExposedPorts(6379);
+        return new GenericContainer<>(DockerImageName.parse("redis:7.4.0")).withExposedPorts(6379);
     }
 
 }
