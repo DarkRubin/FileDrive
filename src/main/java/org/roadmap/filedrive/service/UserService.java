883c71bb06cac7acd69d6efa5911aa48a3212b2f
Vadim <botgugla@gmail.com>
2024-09-04 11:42:06 +0300

    refactor: constructor replaced lombok annotation

--- src/main/java/org/roadmap/filedrive/service/UserService.java
+++ src/main/java/org/roadmap/filedrive/service/UserService.java
@@ -1,18 +1,18 @@
 package org.roadmap.filedrive.service;
 
+import lombok.RequiredArgsConstructor;
 import org.roadmap.filedrive.model.User;
 import org.roadmap.filedrive.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class UserService implements UserDetailsService {
 
-    @Autowired
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
