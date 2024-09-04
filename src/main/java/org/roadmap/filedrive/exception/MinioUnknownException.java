ff5c0dc50ddabfd8b2eae92d384eb29b101c73a7
Vadim <botgugla@gmail.com>
2024-09-04 11:43:44 +0300

    feat: add constructor to MinioUnknownException

--- src/main/java/org/roadmap/filedrive/exception/MinioUnknownException.java
+++ src/main/java/org/roadmap/filedrive/exception/MinioUnknownException.java
@@ -1,4 +1,8 @@
 package org.roadmap.filedrive.exception;
 
 public class MinioUnknownException extends Exception {
+
+    public MinioUnknownException(Throwable cause) {
+        super(cause);
+    }
 }
