147568256c09ac12c0d2ef8179a16a38b2180501
Vadim <botgugla@gmail.com>
2024-09-04 11:52:41 +0300

    feat: MainPage logic added

--- src/main/java/org/roadmap/filedrive/controller/MainPageController.java
+++ src/main/java/org/roadmap/filedrive/controller/MainPageController.java
@@ -1,19 +1,40 @@
 package org.roadmap.filedrive.controller;
 
-import org.springframework.beans.factory.annotation.Value;
+import lombok.RequiredArgsConstructor;
+import org.roadmap.filedrive.exception.MinioUnknownException;
+import org.roadmap.filedrive.service.FileService;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@RequiredArgsConstructor
 public class MainPageController {
 
-    @Value("${spring.application.name}")
-    String appName;
+    private final FileService service;
 
     @GetMapping({"/", "/main"})
     public String mainPage(Model model) {
         model.addAttribute("appName", appName);
+    public String mainPage(Model model, @RequestParam(defaultValue = "") String path) {
+        if (!isAuthenticated()) {
+            return "main-not-auth";
+        }
+        if (path == null || path.isEmpty()) {
+            path = getPath();
+        }
+
+        try {
+            model.addAttribute("files", service.getAllFileNames(path));
+        } catch (MinioUnknownException e) {
+            model.addAttribute("error", "Unknown");
+        }
+        model.addAttribute("path", path);
+        return "main";
+    }
+
         return "main";
     }
 
