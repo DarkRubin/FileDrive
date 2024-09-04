6347621856dba0234eacf82e3c0dc9db4aa9a934
Vadim <botgugla@gmail.com>
2024-09-04 12:02:30 +0300

    feat: SearchPageController added

---
+++ src/main/java/org/roadmap/filedrive/controller/SearchPageController.java
@@ -0,0 +1,27 @@
+package org.roadmap.filedrive.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.roadmap.filedrive.exception.MinioUnknownException;
+import org.roadmap.filedrive.service.FileService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequiredArgsConstructor
+public class SearchPageController {
+
+    private final FileService service;
+
+    @GetMapping("/search")
+    public String searchPage(@RequestParam String input, @RequestParam String path, Model model) {
+        model.addAttribute("searchInput", input);
+        try {
+            model.addAttribute("result", service.search(input, path));
+        } catch (MinioUnknownException e) {
+            model.addAttribute("error", e.getMessage());
+        }
+        return "search";
+    }
+}
