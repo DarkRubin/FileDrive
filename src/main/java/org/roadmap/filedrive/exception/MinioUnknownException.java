99ae4dbf58507f480d9a887de994bfb7348d5be5
Vadim <botgugla@gmail.com>
2024-09-07 11:34:16 +0300

    refactor: MinioUnknownException extend runtimeException

--- src/main/java/org/roadmap/filedrive/exception/MinioUnknownException.java
+++ src/main/java/org/roadmap/filedrive/exception/MinioUnknownException.java
@@ -1,6 +1,6 @@
 package org.roadmap.filedrive.exception;
 
-public class MinioUnknownException extends Exception {
+public class MinioUnknownException extends RuntimeException {
 
     public MinioUnknownException(Throwable cause) {
         super(cause);
