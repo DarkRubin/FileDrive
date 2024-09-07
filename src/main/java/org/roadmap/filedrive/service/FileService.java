c52407d874a16066c4483bc8fe1146b181ec4a53
Vadim <botgugla@gmail.com>
2024-09-07 13:09:21 +0300

    refactor: from file to split repository

--- src/main/java/org/roadmap/filedrive/service/FileService.java
+++ src/main/java/org/roadmap/filedrive/service/FileService.java
@@ -4,9 +4,13 @@ import io.minio.Result;
 import io.minio.messages.Item;
 import lombok.RequiredArgsConstructor;
 import org.roadmap.filedrive.exception.MinioUnknownException;
-import org.roadmap.filedrive.repository.MinioRepository;
+import org.roadmap.filedrive.repository.MinioFileRepository;
+import org.roadmap.filedrive.repository.MinioFolderRepository;
+import org.springframework.core.io.InputStreamResource;
 import org.springframework.stereotype.Service;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
@@ -14,16 +18,20 @@ import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 @Service
 @RequiredArgsConstructor
 public class FileService {
 
-    private final MinioRepository repository;
+    private final MinioFileRepository fileRepository;
+
+    private final MinioFolderRepository folderRepository;
 
     public List<String> getAllFileNames(String folder) throws MinioUnknownException {
         ArrayList<String> result = new ArrayList<>();
-        Iterator<Result<Item>> iterator = repository.getAllFiles(folder).iterator();
+        Iterator<Result<Item>> iterator = folderRepository.getAllContentNames(folder).iterator();
         if (iterator.hasNext()) {
             iterator.next();
         }
@@ -38,24 +46,21 @@ public class FileService {
         return result.reversed();
     }
 
-    public byte[] get(String fullName) throws MinioUnknownException {
-        try {
-            return repository.getFile(fullName).readAllBytes();
-        } catch (IOException e) {
-            throw new MinioUnknownException(e);
-        }
+    public InputStreamResource get(String fullName) throws MinioUnknownException, IOException {
+        var byteStream = new ByteArrayInputStream(fileRepository.get(fullName).readAllBytes());
+        return new InputStreamResource(byteStream);
     }
 
     public void put(String fullName, long length, InputStream file) throws MinioUnknownException, IOException {
-        repository.putFile(fullName, length, file);
+        fileRepository.put(fullName, length, file);
     }
 
     public void delete(String fullName) throws IOException, MinioUnknownException {
-        repository.deleteFile(fullName);
+        fileRepository.delete(fullName);
     }
 
     public void rename(String fullOldName, String fullNewName) throws IOException, MinioUnknownException {
-        repository.updateFile(fullOldName, fullNewName);
+        fileRepository.update(fullOldName, fullNewName);
     }
 
     public List<String> search(String toSearch, String diapason) throws MinioUnknownException {
