81a8b257b1d66a2c9100815a5743e079613c75d7
Vadim <botgugla@gmail.com>
2024-09-10 21:25:58 +0300

    move: SecurityConfig in config folder

--- src/main/java/org/roadmap/filedrive/config/SecurityConfig.java
+++ src/main/java/org/roadmap/filedrive/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package org.roadmap.filedrive.controller;
+package org.roadmap.filedrive.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
