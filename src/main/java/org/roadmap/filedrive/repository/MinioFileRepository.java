a3672067bc9f7019188a476641f31527cbe4fe09
Vadim <botgugla@gmail.com>
2024-09-11 19:10:54 +0300

    feat: add copy method

--- src/main/java/org/roadmap/filedrive/repository/MinioFileRepository.java
+++ src/main/java/org/roadmap/filedrive/repository/MinioFileRepository.java
@@ -1,5 +1,7 @@
 package org.roadmap.filedrive.repository;
 
+import io.minio.CopyObjectArgs;
+import io.minio.CopySource;
 import io.minio.GetObjectArgs;
 import io.minio.GetObjectResponse;
 import io.minio.MinioClient;
@@ -12,7 +14,6 @@ import org.roadmap.filedrive.exception.MinioUnknownException;
 import org.roadmap.filedrive.utils.MinioProperties;
 import org.springframework.stereotype.Repository;
 
-import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.security.InvalidKeyException;
@@ -33,14 +34,14 @@ public class MinioFileRepository {
         bucket = properties.getDefaultBucket();
     }
 
-    public void put(String nameWithPath, long size, InputStream stream) throws IOException, MinioUnknownException {
+    public void put(String nameWithPath, long size, InputStream stream) throws MinioUnknownException {
         try {
             client.putObject(PutObjectArgs.builder()
                     .bucket(bucket)
                     .object(nameWithPath)
                     .stream(stream, size, -1)
                     .build());
-        } catch (MinioException | InvalidKeyException | NoSuchAlgorithmException e) {
+        } catch (MinioException | InvalidKeyException | NoSuchAlgorithmException | IOException e) {
             throw new MinioUnknownException(e);
         }
     }
@@ -56,21 +57,30 @@ public class MinioFileRepository {
         }
     }
 
-    public void delete(String nameWithPath) throws IOException, MinioUnknownException {
+    public void delete(String nameWithPath) throws MinioUnknownException {
         try {
             client.removeObject(RemoveObjectArgs.builder()
                     .bucket(bucket)
                     .object(nameWithPath)
                     .build());
-        } catch (MinioException | InvalidKeyException | NoSuchAlgorithmException e) {
+        } catch (MinioException | InvalidKeyException | NoSuchAlgorithmException | IOException e) {
             throw new MinioUnknownException(e);
         }
     }
 
-    public void update(String oldName, String newName) throws MinioUnknownException, IOException {
-        GetObjectResponse file = get(oldName);
-        byte[] bytes = file.readAllBytes();
-        put(newName, bytes.length, new ByteArrayInputStream(bytes));
+    private void copy(String sourceNameWithPath, String targetNameWithPath) throws MinioUnknownException {
+        try {
+            client.copyObject(CopyObjectArgs.builder()
+                    .bucket(bucket)
+                    .source(CopySource.builder().bucket(bucket).object(sourceNameWithPath).build())
+                    .object(targetNameWithPath).build());
+        } catch (MinioException | InvalidKeyException | IOException | NoSuchAlgorithmException e) {
+            throw new MinioUnknownException(e);
+        }
+    }
+
+    public void update(String oldName, String newName) throws MinioUnknownException {
+        copy(oldName, newName);
         delete(oldName);
     }
 }
